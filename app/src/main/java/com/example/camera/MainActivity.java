59f5ea5f810338f429bbc3f0619c606ea8e9a586
刘伟高 <liuweigao@chinatelecom.cn>
2020-10-15 18:47:17 +0800

    权限申请：相机、存储

--- app/src/main/java/com/example/camera/MainActivity.java
+++ app/src/main/java/com/example/camera/MainActivity.java
@@ -1,14 +1,106 @@
 package com.example.camera;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.app.VoiceInteractor;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final int mRequestCode = 100;//权限请求码
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        checkRequiredPermissions();
+    }
+
+    private void checkRequiredPermissions() {
+        String[] permissions = new String[] {
+                Manifest.permission.CAMERA,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+        };
+        List<String> mPermissionList = new ArrayList<>();
+
+        mPermissionList.clear();
+        for (int i = 0; i < permissions.length; i++) {
+            if (ContextCompat.checkSelfPermission(this, permissions[i]) != PackageManager.PERMISSION_GRANTED) {
+                mPermissionList.add(permissions[i]);
+            }
+        }
+        // 申请权限
+        if (mPermissionList.size() > 0) {
+            ActivityCompat.requestPermissions(this, permissions, mRequestCode);
+        } else {
+            // 权限通过
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                          @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        boolean hasPermissionDismiss = false;
+        if (mRequestCode == requestCode) {
+            for (int i = 0; i < grantResults.length; i++) {
+                if (grantResults[i] == -1) {
+                    hasPermissionDismiss = true;
+                    break;
+                }
+            }
+        }
+        if (hasPermissionDismiss) {
+            // 有些权限没被运行
+            showPermissionDialog();
+        } else {
+            // 权限通过
+        }
+    }
+
+    AlertDialog mPermissionDialog;
+    String mPackName = "com.example.camera";
+    private void showPermissionDialog() {
+
+        if (mPermissionDialog == null) {
+            mPermissionDialog = new AlertDialog.Builder(this)
+                    .setMessage("已禁用权限，请手动授予")
+                    .setPositiveButton("设置", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            cancelPermissionDialog();
+                            Uri packageURI = Uri.parse("package:" + mPackName);
+                            Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS, packageURI);
+                            startActivity(intent);
+                        }
+                    })
+                    .setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            cancelPermissionDialog();
+                            System.exit(0);
+                        }
+                    })
+                    .create();
+        }
+        mPermissionDialog.show();
+    }
+
+    private void cancelPermissionDialog() {
+        mPermissionDialog.cancel();
     }
 }
