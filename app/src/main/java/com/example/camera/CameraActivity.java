fb6cb9e01090b424e4eab7e83b0e1ab1643ec0b1
刘伟高 <liuweigao@chinatelecom.cn>
2020-10-16 14:34:33 +0800

    相机预览

--- app/src/main/java/com/example/camera/CameraActivity.java
+++ app/src/main/java/com/example/camera/CameraActivity.java
@@ -1,32 +1,74 @@
 package com.example.camera;
 
+import android.hardware.Camera;
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
-import android.view.View;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 
-public class CameraActivity extends AppCompatActivity {
+public class CameraActivity extends AppCompatActivity implements SurfaceHolder.Callback {
+
+    private final String TAG = "CameraActivity";
+
+    private SurfaceView mCameraSurface;
+    private SurfaceHolder mHolder;
+
+
+    private Camera mCamera = null;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+        mCameraSurface = findViewById(R.id.camera_preview);
+        mHolder = mCameraSurface.getHolder();
+        mHolder.addCallback(CameraActivity.this);
+    }
+
+    // camera preview
+    private Camera getCamera(int cameraId) {
+        Camera camera;
+        try {
+            camera = Camera.open(cameraId);
+        } catch (Exception e) {
+            camera = null;
+        }
+        return camera;
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        try {
+            mCamera = getCamera(0);
+            mCamera.setPreviewDisplay(holder);
+            mCamera.setDisplayOrientation(90);
+            mCamera.startPreview();
+        } catch (Exception e) {
+            finish();
+        }
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        if (mCamera != null) {
+            try {
+                mCamera.setPreviewCallback(null);
+                mCamera.stopPreview();
+                mCamera.release();
+                mCamera = null;
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-        });
+        }
     }
 
 }
